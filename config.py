24d21c39b67c4a635ca05dd2ca15628603fc8368
RitikKaal <157566142+RitikKaal@users.noreply.github.com>
2024-06-28 13:45:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002170965430))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002249580335))
 
 # ------------------------------------------------
 GPT_API = getenv("GPT_API")
